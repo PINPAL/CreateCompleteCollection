043e29409b7fbff6b7d59603848147bc83d36b7c
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-08-25 11:08:33 +0100

    Cheaper leather stitching

--- kubejs/server_scripts/tools_and_armor/early_game_crafting/armor_stitching.js
+++ kubejs/server_scripts/tools_and_armor/early_game_crafting/armor_stitching.js
@@ -1,15 +1,21 @@
 ServerEvents.recipes((event) => {
-  event
-    .shaped("kubejs:leather_stitching", ["M M", "MSM", "M M"], {
-      M: "#forge:leather",
-      S: "#forge:string",
-    })
-    .id("kubejs:leather_stitching");
+	event
+		.shaped("kubejs:leather_stitching", ["   ", "MSM", "   "], {
+			M: "#forge:leather",
+			S: "#forge:string",
+		})
+		.id("kubejs:leather_stitching");
+	event
+		.shaped("kubejs:leather_stitching", [" M ", " S ", " M "], {
+			M: "#forge:leather",
+			S: "#forge:string",
+		})
+		.id("kubejs:leather_stitching_vert");
 
-  event
-    .shaped("kubejs:chainmail_stitching", ["M M", "MSM", "M M"], {
-      M: "minecraft:chain",
-      S: "#forge:string",
-    })
-    .id("kubejs:chainmail_stitching");
+	event
+		.shaped("kubejs:chainmail_stitching", ["M M", "MSM", "M M"], {
+			M: "minecraft:chain",
+			S: "#forge:string",
+		})
+		.id("kubejs:chainmail_stitching");
 });
