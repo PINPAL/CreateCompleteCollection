83e2b31c77f4db73ccfe830620157a391213d919
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-08-27 12:06:18 +0100

    Cheaper Leather Armor

--- kubejs/server_scripts/recipes/tools_and_armor/early_game_crafting/leather_armor.js
+++ kubejs/server_scripts/recipes/tools_and_armor/early_game_crafting/leather_armor.js
@@ -1,15 +1,19 @@
 ServerEvents.recipes((event) => {
-  // Leather Armor
-  event.shaped("minecraft:leather_helmet", ["AAA", "A A"], {
-    A: "#forge:leather",
-  });
-  event.shaped("minecraft:leather_chestplate", ["A A", "AAA", "AAA"], {
-    A: "#forge:leather",
-  });
-  event.shaped("minecraft:leather_leggings", ["AAA", "A A", "A A"], {
-    A: "#forge:leather",
-  });
-  event.shaped("minecraft:leather_boots", ["A A", "A A"], {
-    A: "#forge:leather",
-  });
+	// Leather Armor
+	event.shaped("minecraft:leather_helmet", ["SAS", "A A"], {
+		A: "#forge:leather",
+		S: "#forge:rods/wooden",
+	});
+	event.shaped("minecraft:leather_chestplate", ["A A", "ASA", "SAS"], {
+		A: "#forge:leather",
+		S: "#forge:rods/wooden",
+	});
+	event.shaped("minecraft:leather_leggings", ["ASA", "S S", "A A"], {
+		A: "#forge:leather",
+		S: "#forge:rods/wooden",
+	});
+	event.shaped("minecraft:leather_boots", ["S S", "A A"], {
+		A: "#forge:leather",
+		S: "#forge:rods/wooden",
+	});
 });
