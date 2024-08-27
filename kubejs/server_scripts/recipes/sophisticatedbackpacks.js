e8d54a41d415deae58b0a790c0ec514511296fe8
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-08-27 11:22:16 +0100

    Refactor some tools/armor logic

--- kubejs/server_scripts/recipes/sophisticatedbackpacks.js
+++ kubejs/server_scripts/recipes/sophisticatedbackpacks.js
@@ -1,4 +1,45 @@
+function upgradeBackpack(inventory, itemstack, backpack) {
+	let item = inventory.find(Item.of(backpack).ignoreNBT());
+	let nbt = item.nbt;
+	if (nbt == null) {
+		nbt = {};
+	} else {
+		nbt.inventorySlots = nbt.inventorySlots + 9;
+		nbt.upgradeSlots = nbt.upgradeSlots + 1;
+	}
+	return itemstack.withNBT(nbt);
+}
+
+const backpackTiers = [
+	{ backpackId: "iron", previousTier: "", stitching: "iron" },
+	{
+		backpackId: "gold",
+		previousTier: "iron",
+		stitching: "steel",
+	},
+	{
+		backpackId: "diamond",
+		previousTier: "gold",
+		stitching: "diamond",
+	},
+	{
+		backpackId: "netherite",
+		previousTier: "diamond",
+		stitching: "netherite",
+	},
+];
+
 ServerEvents.recipes((event) => {
+	// Backpack Upgrading
+	backpackTiers.forEach((tier) => {
+		let output = `sophisticatedbackpacks:${tier.backpackId}_backpack`;
+		let input = `sophisticatedbackpacks:${tier.previousTier}${tier.previousTier == "" ? "" : "_"}backpack`;
+		let stitching = `2x kubejs:${tier.stitching}_stitching`;
+		event.shapeless(output, [input, stitching]).modifyResult((inventory, itemstack) => {
+			return upgradeBackpack(inventory, itemstack, input);
+		});
+	});
+
 	// Backpack Base
 	event.shaped("sophisticatedbackpacks:backpack", ["S8S", "SCS", "L8L"], {
 		S: "#forge:string",
