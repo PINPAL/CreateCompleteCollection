1de5f0cf8639d755646e10936fa1498c3ea86237
PINPAL <18197217+PINPAL@users.noreply.github.com>
2024-09-02 14:43:04 +0100

    Bump Version 515

--- update_version.py
+++ update_version.py
@@ -8,7 +8,7 @@ commits = int(os.popen("git rev-list --count HEAD").read().strip())
 new_version = f"1.6.{commits + 1}"
 
 # Function to find and replace the version number in a config file
-def replace_version(config_path, new_version, line_heading, needsPunctuation=''):
+def replace_version(config_path, new_version, line_heading, needsPunctuation='', needsTab=""):
     config_path = os.path.join(os.path.dirname(script_path), config_path)
     with open(config_path, "r") as f:
         config = f.read()
@@ -16,7 +16,7 @@ def replace_version(config_path, new_version, line_heading, needsPunctuation='')
             # print(line)
             # find the line that contains the version number
             if line_heading in line:
-                new_line = f'{line_heading}{needsPunctuation}{new_version}{needsPunctuation}'
+                new_line = f'{"\t" if needsTab else ""}{line_heading}{needsPunctuation}{new_version}{needsPunctuation}'
                 config = config.replace(line, new_line)
                 # write the updated config file
                 with open(config_path, "w") as f:
@@ -27,5 +27,5 @@ def replace_version(config_path, new_version, line_heading, needsPunctuation='')
                 
         print("Failed to find version number in config file")
 
-replace_version("config/bcc-common.toml", new_version, "modpackVersion = ", needsPunctuation='"')
+replace_version("config/bcc-common.toml", new_version, "modpackVersion = ", needsPunctuation='"', needsTab=True)
 replace_version("config/fancymenu/customization/create-mod.txt", new_version, "source = Create Complete Collection (LEGACY): v")
